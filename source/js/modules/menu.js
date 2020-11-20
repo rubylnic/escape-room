d5cb0c532596cb187e3585d343663b16c26bc77f
Ksenia <ksenia.bolshako@mail.ru>
2020-11-20 15:44:51 +0300

    'for-check'

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,28 +1,27 @@
 'use strict';
-(function() {
-    var btn = document.querySelector('.main-header__menu-button');
-    var bg = document.querySelector('.background-container');
-    var nav = document.querySelector('.main-nav');
-    var main = document.querySelector('.main');
-    var description = document.querySelector('.description');
-    var footer = document.querySelector('.footer');
+(function () {
+  var btn = document.querySelector('.main-header__menu-button');
+  var bg = document.querySelector('.background-container');
+  var nav = document.querySelector('.main-nav');
+  var main = document.querySelector('.main');
+  var footer = document.querySelector('.footer');
 
-    var js = function() {
-        bg.classList.remove('background-container--opened');
-        nav.classList.add('main-nav--closed');
-        btn.classList.add('main-header__menu-button--closed');
-        footer.classList.add('footer--closed');
-        main.classList.remove('main--closed');
-    }
-    js();
+  var js = function () {
+    bg.classList.remove('background-container--opened');
+    nav.classList.add('main-nav--closed');
+    btn.classList.add('main-header__menu-button--closed');
+    footer.classList.add('footer--closed');
+    main.classList.remove('main--closed');
+  };
+  js();
 
-    var opencloseMenu = function(evt) {
-        evt.preventDefault();
-        bg.classList.toggle('background-container--opened');
-        nav.classList.toggle('main-nav--closed');
-        btn.classList.toggle('main-header__menu-button--closed');
-        footer.classList.toggle('footer--closed');
-        main.classList.toggle('main--closed');
-    }
-    btn.addEventListener('click', opencloseMenu);
-})()
+  var openCloseMenu = function (evt) {
+    evt.preventDefault();
+    bg.classList.toggle('background-container--opened');
+    nav.classList.toggle('main-nav--closed');
+    btn.classList.toggle('main-header__menu-button--closed');
+    footer.classList.toggle('footer--closed');
+    main.classList.toggle('main--closed');
+  };
+  btn.addEventListener('click', openCloseMenu);
+})();
